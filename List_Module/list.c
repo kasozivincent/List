3463f2251e266e5781888dd5bc5f431981e3949f
kasozi vincent <kasozivincentug507@gmail.com>
2023-02-17 13:02:00 +0000

    Add project files

---
+++ List_Module/list.c
@@ -0,0 +1,17 @@
+#include "list.h"
+
+struct _node{
+    Data content;
+    struct _list* tail;
+};
+
+struct _list{
+   Node head;
+};
+
+List empty_list(){
+    List listptr = malloc(sizeof(struct _list));
+    if(listptr == None) exit(1);
+    *listptr = (struct _list){.head = None};
+    return listptr; 
+}
---
+++ List_Module/list.h
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdbool.h>
+#include<stdio.h>
+
+#define None NULL
+
+struct _list;
+typedef void* Data;
+typedef struct _node* Node;
+typedef struct _list* List;
+
+List empty_list();
